b0a10c15517464695462e4e0a542bafb8d6b8585
tanveerali <tanveerali.shaikh@flaconi.de>
2025-07-15 17:35:37 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Remove or set to ALLOWALL to permit embedding
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
